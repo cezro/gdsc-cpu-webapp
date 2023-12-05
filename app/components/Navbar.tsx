4e4de844a8f391037c0b97554722a5d1fdcc72b0
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2023-12-05 16:42:30 +0800

    [Fix][Teddie] fixed text field

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Navbar(){
+  return (
+    <nav className="h-16 bg-white shadow-md">
+      <div className="flex flex-row"></div>
+    </nav>
+  )
+}
