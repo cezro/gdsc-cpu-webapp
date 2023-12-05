6c71a0209eb511ce9679ec1b4ac132d0d0bf5564
cezro <hyderz1218@gmail.com>
2023-12-05 16:59:04 +0800

    deleted navbar

--- app/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Navbar(){
-  return (
-    <nav className="h-16 bg-white shadow-md">
-      <div className="flex flex-row"></div>
-    </nav>
-  )
-}
