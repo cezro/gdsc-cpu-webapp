ca8de12906b0eadf78cc685f04ac6d30820b6700
Rex <rex.enojas-22@cpu.edu.ph>
2023-12-08 06:32:08 +0800

    [Feature][Rex] added signup page UI

---
+++ app/sign_up/page.tsx
@@ -0,0 +1,12 @@
+import Signup from './signup'
+import Layout from './layout'
+
+export default function SignUp() {
+  return (
+    <Layout>
+    <main>
+      <Signup></Signup>
+    </main>
+    </Layout>
+  )
+}
