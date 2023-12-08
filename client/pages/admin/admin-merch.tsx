481164cdec417261557758d6b9300f0b432c1071
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2023-12-08 21:46:45 +0800

    [Feature][Ted] added admin panel merch section UI

--- client/pages/admin/admin-merch.tsx
+++ client/pages/admin/admin-merch.tsx
@@ -4,8 +4,24 @@ import Layout from '../../components/Layout';
 export default function AdminMerch() {
   return (
     <Layout>
-      <h1 className="text-stone-900">Edit Merch</h1>
-      {/*home page content */}
+      <h1 className="text-black text-3xl mb-6">Create GDSC merch</h1>
+      <div className="flex flex-col space-y-4">
+        <div>
+          <p className="text-black text-md font-semibold">Merch Name</p>
+          <input className='text-black w-64 border border-stone-900' placeholder='Name'/>
+        </div>
+        <div>
+          <p className="text-black text-md font-semibold">Merch Photo</p>
+          <input type='file' className='text-black'/>
+        </div>
+        <div>
+          <p className="text-black text-md font-semibold">Add a price to your merch</p>
+          <input className='text-black w-64 border border-stone-900' placeholder='Price'/>
+        </div>
+        <div>
+          <button className='h-12 w-36 bg-blue-600 border rounded-xl hover:bg-blue-800'>Publish Merch</button>
+        </div>
+      </div>
     </Layout>
   );
 }
