649a72c0fa77eb07f4993a68f6c5e3d593a15bda
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2023-12-21 21:13:22 +0800

    [Ted][Add] added routing for admin-panel

--- server/server.ts
+++ server/server.ts
@@ -4,9 +4,14 @@ import express from 'express';
 import cors from 'cors';
 const PORT = 3001;
 
+async function serverStart() {
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}));
 
 const randomName = faker.person.fullName(); // Rowan Nikolaus
 const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
@@ -21,6 +26,21 @@ app.get('/db/test', (req, res) => {
   res.json({ message: pool });
 });
 
+app.get('/admin/admin-home', (req, res) => {
+  res.json({ message: 'This is the admin home panel'})
+})
+
+app.get('/admin/admin-merch', (req, res) => {
+  res.json({ message: 'This is the admin merch panel'})
+})
+
+app.get('/admin/admin-events', (req, res) => {
+  res.json({ message: 'This is the admin events panel'})
+})
+
 app.listen(PORT, () => {
   console.log('Server started on', PORT);
 });
+}
+
+serverStart()
