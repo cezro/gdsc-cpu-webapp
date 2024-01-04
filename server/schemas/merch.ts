c8c2283e71f62fd6ce8d468500be9e0afac4660f
cezro <hyderz1218@gmail.com>
2024-01-05 04:58:57 +0800

    moved schemas to appropriate directories

--- server/schemas/merch.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const merchSchema = z.object({
-  name: z.string().min(1, { message: 'Missing Event Name' }),
-  description: z.string().min(1, { message: 'Missing Description' }),
-  price: z.number().min(1),
-});
