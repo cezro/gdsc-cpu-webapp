57a167b73104a4f826b86f1cefc40977330e864f
cezro <hyderz1218@gmail.com>
2024-01-05 04:58:57 +0800

    moved schemas to appropriate directories

--- server/schemas/user.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const userSchema = z.object({
-  fname: z.string().min(1, { message: 'Missing First Name' }),
-  lname: z.string().min(1, { message: 'Missing Last Name' }),
-  email: z.string().min(1, { message: 'Missing Email' }).email('Invalid email'),
-  password: z.string().min(1, { message: 'Missing Password' }),
-});
