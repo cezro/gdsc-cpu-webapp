ec529a2b8ec4becbd57ba0d5efa1beebe85ce545
cezro <hyderz1218@gmail.com>
2024-01-05 21:11:43 +0800

    updated zod schemas in frontend

--- client/schemas/user.ts
+++ client/schemas/user.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
 export const userSchema = z.object({
-  fname: z.string().min(1, { message: 'Missing First Name' }),
-  lname: z.string().min(1, { message: 'Missing Last Name' }),
+  fname: z
+    .string()
+    .min(1, { message: 'Missing First Name' })
+    .max(255, 'Reached Character Limit'),
+  lname: z
+    .string()
+    .min(1, { message: 'Missing Last Name' })
+    .max(255, 'Reached Character Limit'),
   email: z.string().min(1, { message: 'Missing Email' }).email('Invalid email'),
   password: z.string().min(1, { message: 'Missing Password' }),
 });
